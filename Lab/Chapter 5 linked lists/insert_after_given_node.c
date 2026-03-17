056d7937716103146801f7a4eae1faea0878f443
Md. Maruf Mia <mdmarufmia026@gmail.com>
2026-03-17 10:10:40 +0600

    Add files via upload

---
+++ Lab/Chapter 5 linked lists/insert_after_given_node.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+    int data ;
+    struct node*next;
+};
+struct node*insert_prenode(struct node*head,int data,struct node*pre_node )
+{
+struct node*ptr;
+ptr=(struct node*)malloc(sizeof(struct node));
+ptr->data=data;
+ptr->next=pre_node->next;
+pre_node->next=ptr;
+head=ptr;
+return head;
+}
+int main()
+{
+     struct node*head;
+struct node*n1;
+struct node*n2;
+struct node*n3;
+
+head=(struct node*)malloc(sizeof(struct node));
+n1=(struct node*)malloc(sizeof(struct node));
+n2=(struct node*)malloc(sizeof(struct node));
+n3=(struct node*)malloc(sizeof(struct node));
+
+head->data=11;
+head->next=n1;
+
+n1->data=14;
+n1->next=n2;
+
+n2->data=13;
+n2->next=n3;
+
+n3->data=34;
+n3->next=NULL;
+insert_prenode(head,25,n2);
+struct node *ptr=head;
+while(ptr!=NULL)
+{
+    printf("%d\n",ptr->data);
+    ptr=ptr->next;
+}
+return 0;
+}
