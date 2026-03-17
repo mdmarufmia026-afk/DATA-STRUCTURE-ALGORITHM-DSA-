a21b5d9e51f6eef5e853c00c16f65d31d79507d9
Md. Maruf Mia <mdmarufmia026@gmail.com>
2026-03-17 09:57:39 +0600

    Add files via upload

---
+++ Lab/Chapter 5 linked lists/insert_last.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+    int data ;
+    struct node*next;
+};
+struct node*insert_last(struct node*head,int data)
+{
+struct node*ptr;
+ptr=(struct node*)malloc(sizeof(struct node));
+struct node*p=head;
+while(p->next!=NULL)
+{p=p->next;
+}
+
+p->next=ptr;
+ptr->next=NULL;
+ptr->data=data;
+return head;
+}
+
+int main()
+{
+     struct node*head;
+struct node*n1;
+struct node*n2;
+struct node*n3;
+
+head=(struct node*)malloc(sizeof(struct node));
+n1=(struct node*)malloc(sizeof(struct node));
+n2=(struct node*)malloc(sizeof(struct node));
+n3=(struct node*)malloc(sizeof(struct node));
+
+head->data=11;
+head->next=n1;
+
+n1->data=14;
+n1->next=n2;
+
+n2->data=13;
+n2->next=n3;
+
+n3->data=34;
+n3->next=NULL;
+insert_last(head,23);
+struct node *ptr=head;
+while(ptr!=NULL)
+{
+    printf("%d\n",ptr->data);
+    ptr=ptr->next;
+}
+return 0;
+}
