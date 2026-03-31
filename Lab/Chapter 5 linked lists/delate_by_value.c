2acad0bf9e966b6fcf0869aa0e58ab069d6df5f5
Md. Maruf Mia <mdmarufmia026@gmail.com>
2026-03-31 22:36:35 +0600

    Add files via upload

---
+++ Lab/Chapter 5 linked lists/delate_by_value.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+    int data ;
+    struct node*next;
+};
+struct node*delete_value(struct node*head, int value)
+{
+struct node*p=head;
+struct node*q=head->next;
+int i=0;
+while(q!=NULL&&q->data!=value)
+{
+    p=p->next;
+    q=q->next; 
+}
+if(q!=NULL){
+p->next=q->next;
+free(q);}
+return head;
+}
+int main()
+{
+     struct node*head;
+struct node*n1;
+struct node*n2;
+struct node*n3;
+
+head=(struct node*)malloc(sizeof(struct node));
+n1=(struct node*)malloc(sizeof(struct node));
+n2=(struct node*)malloc(sizeof(struct node));
+n3=(struct node*)malloc(sizeof(struct node));
+
+head->data=11;
+head->next=n1;
+
+n1->data=14;
+n1->next=n2;
+
+n2->data=13;
+n2->next=n3;
+
+n3->data=34;
+n3->next=NULL;
+head=delete_value(head,13);
+struct node *ptr=head;
+
+while(ptr!=NULL)
+{
+    printf("%d\n",ptr->data);
+    ptr=ptr->next;
+}
+return 0;
+}
