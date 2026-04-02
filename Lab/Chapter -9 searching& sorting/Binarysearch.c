c788f000e4850c8b03aa224bbc6994543166dbb2
Md. Maruf Mia <mdmarufmia026@gmail.com>
2026-04-02 12:16:56 +0600

    Add files via upload

---
+++ Lab/Chapter -9 searching& sorting/Binarysearch.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int data[]={34,40,45,46,56,70};
+    int beg=0,n=6,end=n-1,item =46;
+    //printf("Enter item to be searched: ");
+    while (beg<=end)
+    {
+      int mid=((beg+end)/2);
+      if(data[mid]==item)
+      { 
+       printf("%d",data[mid]);
+       break;
+      }
+   else if(data[mid]<item)
+    {
+    beg=mid+1;
+    }
+   else if(data[mid]>item)
+    {
+    end=mid-1;
+    }
+
+    }
+    if (beg>end)
+    printf("null");
+
+
+
+ return 0;
+}
